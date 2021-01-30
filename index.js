9e8c118eea79501c725b34f1f3930c69e74e702f
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2021-01-30 14:58:34 -0500

    commit tree ca3f243fa71850b87f8117fd94e0be9ef609a797

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(strName, evName) {
+    let thankYouCards = []
+    for (let i = 0; i < strName.length; i++) {
+        thankYouCards.push(`Thank you, ${strName[i]}, for the wonderful ${evName} gift!`)
+    }
+    return thankYouCards
+}
+
+function countDown(num) {
+    while (num > 0) {
+      console.log(num);
+      num -= 1;
+    }
+    console.log(num);
+  }
